c461ed0627b8f139b8d9ccf706093352408f0c90
iso <ismailkurtlojistik@gmail.com>
2023-04-09 02:34:54 +0300

    todo

--- src/data.js
+++ src/data.js
@@ -6,48 +6,54 @@ import router from "./img/router_page.png";
 import shipping from "./img/shipping.png";
 import pokemon from "./img/pokemon_api.png";
 import kredi from "./img/kredi_hesaplama.png";
+import todo from "./img/todo.png";
 
 export const products = [
   {
     id: 1,
-    link: "https://tour-place-qf4qltjtn-kurtismail.vercel.app/",
-    img: tour,
-  },
-  {
-    id: 2,
-    link: "https://recipe-api-react.vercel.app/",
-    img: recipe,
-  },
-  {
-    id: 3,
     link: "https://movie-app-firebase-tailwind-i74owwpzp-kurtismail.vercel.app/",
     img: movie,
   },
   {
+    id: 2,
+    link: "https://kurtismail.github.io/todo-app/",
+    img: todo,
+  },
+  {
+    id: 3,
+    link: "https://tour-place-qf4qltjtn-kurtismail.vercel.app/",
+    img: tour,
+  },
+  {
     id: 4,
+    link: "https://recipe-api-react.vercel.app/",
+    img: recipe,
+  },
+  {
+    id: 5,
     link: "https://kurtismail.github.io/weather-app/",
     img: weather,
   },
 
   {
-    id: 5,
+    id: 6,
     link: "https://router-page.vercel.app/",
     img: router,
   },
   {
-    id: 6,
+    id: 7,
     link: "https://kurtismail.github.io/check-out-shipping/",
     img: shipping,
   },
 
   {
-    id: 7,
+    id: 8,
     link: "https://kurtismail.github.io/pokemon_study/",
     img: pokemon,
   },
 
   {
-    id: 8,
+    id: 9,
     link: "https://kurtismail.github.io/kredi_hesaplama_project/",
     img: kredi,
   },
